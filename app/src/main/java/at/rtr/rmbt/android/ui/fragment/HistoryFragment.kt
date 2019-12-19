5d60701a1f3c1d8137a10dbd362a48e7cdfaacb3
nikul@vervesys <nikul@vervesys.com>
2019-12-19 20:19:42 +0530

    Add error handling functionality Change error message

--- app/src/main/java/at/rtr/rmbt/android/ui/fragment/HistoryFragment.kt
+++ app/src/main/java/at/rtr/rmbt/android/ui/fragment/HistoryFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
+import at.rmbt.util.exception.HandledException
+import at.rmbt.util.exception.NoConnectionException
 import at.rtr.rmbt.android.R
 import at.rtr.rmbt.android.databinding.FragmentHistoryBinding
 import at.rtr.rmbt.android.di.viewModelLazy
 import at.rtr.rmbt.android.ui.adapter.HistoryAdapter
+import at.rtr.rmbt.android.ui.dialog.SimpleDialog
 import at.rtr.rmbt.android.util.ToolbarTheme
 import at.rtr.rmbt.android.util.changeStatusBarColor
 import at.rtr.rmbt.android.util.listen
 import at.rtr.rmbt.android.viewmodel.HistoryViewModel
 
+private const val CODE_ERROR = 1
 class HistoryFragment : BaseFragment() {
 
     private val historyViewModel: HistoryViewModel by viewModelLazy()
@@ -55,9 +59,32 @@ class HistoryFragment : BaseFragment() {
         activity?.window?.changeStatusBarColor(ToolbarTheme.WHITE)
     }
 
+    override fun onHandledException(exception: HandledException) {
+
+        val messageTextResId = when (exception) {
+            is NoConnectionException -> {
+                R.string.no_internet_connection_not_load_data
+            }
+            else -> {
+                R.string.test_dialog_error_text
+            }
+        }
+
+        fragmentManager?.let {
+            SimpleDialog.Builder()
+                .messageText(messageTextResId)
+                .positiveText(R.string.input_setting_dialog_ok)
+                .cancelable(true)
+                .show(it, CODE_ERROR)
+        }
+    }
     override fun onStart() {
         super.onStart()
 
-        historyViewModel.clearHistory()
+        historyViewModel.historyLiveData.value?.let {
+            if (it.isEmpty()) {
+                historyViewModel.clearHistory()
+            }
+        }
     }
 }
