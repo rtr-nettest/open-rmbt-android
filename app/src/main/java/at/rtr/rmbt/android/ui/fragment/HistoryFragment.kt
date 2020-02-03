e3ba5434f6bea9bc14cbfb420d22b49f27ced8c5
Alexander Sokol <sokol.a@dbbest.com>
2020-02-03 14:42:43 +0200

    fix connection error translation

--- app/src/main/java/at/rtr/rmbt/android/ui/fragment/HistoryFragment.kt
+++ app/src/main/java/at/rtr/rmbt/android/ui/fragment/HistoryFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
-import at.rmbt.util.exception.HandledException
-import at.rmbt.util.exception.NoConnectionException
 import at.rtr.rmbt.android.R
 import at.rtr.rmbt.android.databinding.FragmentHistoryBinding
 import at.rtr.rmbt.android.di.viewModelLazy
 import at.rtr.rmbt.android.ui.activity.ResultsActivity
 import at.rtr.rmbt.android.ui.adapter.HistoryAdapter
-import at.rtr.rmbt.android.ui.dialog.SimpleDialog
 import at.rtr.rmbt.android.ui.dialog.SyncDevicesDialog
 import at.rtr.rmbt.android.util.ToolbarTheme
 import at.rtr.rmbt.android.util.changeStatusBarColor
@@ -74,24 +71,5 @@ class HistoryFragment : BaseFragment() {
     private fun refreshHistory() {
         historyViewModel.refreshHistory()
         binding.swipeRefreshLayoutHistoryItems.isRefreshing = false
-    }
-
-    override fun onHandledException(exception: HandledException) {
-        val messageTextResId = when (exception) {
-            is NoConnectionException -> {
-                R.string.no_internet_connection_not_load_data
-            }
-            else -> {
-                R.string.test_dialog_error_text
-            }
-        }
-
-        fragmentManager?.let {
-            SimpleDialog.Builder()
-                .messageText(messageTextResId)
-                .positiveText(R.string.input_setting_dialog_ok)
-                .cancelable(true)
-                .show(it, CODE_ERROR)
-        }
     }
 }
