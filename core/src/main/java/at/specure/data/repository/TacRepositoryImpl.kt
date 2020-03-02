f86f37bf7e955ed3548f79334a8e1d56fd332592
Michal Cádrik <michal.cadrik@martes-specure.com>
2020-03-02 11:04:39 +0000

    fixed core review comments

--- core/src/main/java/at/specure/data/repository/TacRepositoryImpl.kt
+++ core/src/main/java/at/specure/data/repository/TacRepositoryImpl.kt
@@ -7,6 +7,7 @@ import at.specure.data.entity.TacRecord
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.net.URL
@@ -17,47 +18,40 @@ class TacRepositoryImpl(db: CoreDatabase, private val termsAndConditions: TermsA
 
     override fun getTac(language: String): Flow<TacRecord?> = flow {
         val tacRecord = getTacFromDB(language)
-
-        if (tacRecord != null && termsAndConditions.tacVersion ?: -1 <= tacRecord.version) {
-            emit(tacRecord)
-        } else {
+        if (tacRecord == null || termsAndConditions.tacVersion ?: -1 > tacRecord.version) {
             val contentFromServerLink = loadFromHtml(termsAndConditions.tacUrl ?: "")
-            if (contentFromServerLink.isNotEmpty()) {
-                val record = TacRecord(language = language, version = termsAndConditions.tacVersion ?: -1, content = contentFromServerLink)
-                emit(record)
-                saveTac(record)
-            } else {
-                if (tacRecord != null && tacRecord.content.isNotEmpty()) {
-                    emit(tacRecord)
-                } else {
+            if (contentFromServerLink == null) {
+                if (tacRecord == null || tacRecord.content.isEmpty()) {
                     val contentFromLocalLink = loadFromHtml(termsAndConditions.defaultUrl)
-                    if (contentFromLocalLink.isNotEmpty()) {
+                    if (contentFromLocalLink == null) {
+                        val contentFromLocalFile = parseAndPrepareHtml(termsAndConditions.localVersion)
+                        val record = TacRecord(language, -1, contentFromLocalFile)
+                        emit(record)
+                    } else {
                         val record = TacRecord(language = language, version = -1, content = contentFromLocalLink)
                         emit(record)
                         saveTac(record)
-                    } else {
-                        val contentFromLocalFile = loadFromLocal()
-                        val record = TacRecord(language, -1, contentFromLocalFile)
-                        emit(record)
                     }
+                } else {
+                    emit(tacRecord)
                 }
+            } else {
+                val record = TacRecord(language = language, version = termsAndConditions.tacVersion ?: -1, content = contentFromServerLink)
+                emit(record)
+                saveTac(record)
             }
+        } else {
+            emit(tacRecord)
         }
     }
 
-    private fun loadFromLocal(): String {
-        return try {
-            parseAndPrepareHtml(termsAndConditions.localVersion)
-        } catch (e: Exception) {
-            ""
-        }
-    }
-
-    private fun loadFromHtml(url: String): String {
+    private fun loadFromHtml(url: String): String? {
         return try {
             parseAndPrepareHtml(URL(url).openStream())
-        } catch (e: Exception) {
-            ""
+        } catch (e: IOException) {
+            null
+        } catch (e: IllegalArgumentException) {
+            null
         }
     }
 
