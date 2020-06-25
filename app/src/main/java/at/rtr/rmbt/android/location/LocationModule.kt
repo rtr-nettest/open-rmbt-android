96a1a1f212e33a9b564cb935b4d9546e141ac90c
Thomas Schreiber <thomas.schreiber@gmx.at>
2020-06-25 16:29:09 +0200

    translation fix

--- app/src/main/java/at/rtr/rmbt/android/location/LocationModule.kt
+++ app/src/main/java/at/rtr/rmbt/android/location/LocationModule.kt
@@ -1,7 +1,7 @@
 package at.rtr.rmbt.android.location
 
 import android.content.Context
-import at.specure.location.FusedLocationSource
+//import at.specure.location.FusedLocationSource
 import at.specure.location.GPSLocationSource
 import at.specure.location.LocationWatcher
 import at.specure.location.NetworkLocationSource
@@ -20,6 +20,6 @@ class LocationModule {
     fun provideLocationProvider(context: Context): LocationWatcher = LocationWatcher.Builder(context)
         .addSource(GPSLocationSource(context))
         .addSource(NetworkLocationSource(context))
-        .addSource(FusedLocationSource(context))
+        //.addSource(FusedLocationSource(context))
         .build()
 }
