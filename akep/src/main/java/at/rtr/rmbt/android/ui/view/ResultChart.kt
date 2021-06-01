8baa05661befd823904f50126a7a0b50eaba6e4b
michal cadrik <michal.cadrik@martes-specure.com>
2021-06-01 15:45:11 +0200

    removed unused code

--- akep/src/main/java/at/rtr/rmbt/android/ui/view/ResultChart.kt
+++
@@ -1,9 +0,0 @@
-package at.rtr.rmbt.android.ui.view
-
-import at.specure.data.NetworkTypeCompat
-import at.specure.data.entity.TestResultGraphItemRecord
-
-interface ResultChart {
-
-    fun addResultGraphItems(graphItems: List<TestResultGraphItemRecord>?, networkType: NetworkTypeCompat)
-}
