23302519c7ba03b506235fe80f809294341dbfaa
michal cadrik <michal.cadrik@martes-specure.com>
2021-06-01 15:15:36 +0200

    removed unused files

--- akep/src/main/java/at/rtr/rmbt/android/util/exception/UnknownException.kt
+++
@@ -1,6 +0,0 @@
-package at.rtr.rmbt.android.util.exception
-
-import at.rmbt.util.exception.HandledException
-import at.rtr.rmbt.android.R
-
-class UnknownException : HandledException(R.string.error_unknown)
