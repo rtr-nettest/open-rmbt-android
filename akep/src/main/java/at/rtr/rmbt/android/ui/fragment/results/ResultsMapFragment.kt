dff6a985bf40e0cae6b8aa2ab91c64b6b0c53799
michal cadrik <michal.cadrik@martes-specure.com>
2021-08-26 11:51:04 +0200

    removed unused code and little fixes

--- akep/src/main/java/at/rtr/rmbt/android/ui/fragment/results/ResultsMapFragment.kt
+++
@@ -1,8 +0,0 @@
-package at.rtr.rmbt.android.ui.fragment.results
-
-import at.rtr.rmbt.android.R
-import at.rtr.rmbt.android.ui.fragment.BaseFragment
-
-class ResultsMapFragment : BaseFragment() {
-    override val layoutResId: Int = R.layout.fragment_results_map
-}
