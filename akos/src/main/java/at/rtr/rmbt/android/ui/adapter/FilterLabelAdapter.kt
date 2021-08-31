eefa76f0bca4d188bd19b32686c701aa8d2988c9
michal cadrik <michal.cadrik@martes-specure.com>
2021-08-31 13:12:14 +0200

    ONT-3546 - prepared AKOS (slovenia) dev version

---
+++ akos/src/main/java/at/rtr/rmbt/android/ui/adapter/FilterLabelAdapter.kt
@@ -0,0 +1,32 @@
+package at.rtr.rmbt.android.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import at.rtr.rmbt.android.R
+import at.rtr.rmbt.android.databinding.ItemFilterLabelBinding
+import at.rtr.rmbt.android.util.bindWith
+
+class FilterLabelAdapter(private val onRemove: (String) -> Unit) : RecyclerView.Adapter<FilterLabelAdapter.Holder>() {
+
+    private val _items = mutableListOf<String>()
+
+    var items: Set<String>
+        get() = _items.toSet()
+        set(value) {
+            _items.clear()
+            _items.addAll(value)
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = Holder(parent.bindWith(R.layout.item_filter_label))
+
+    override fun getItemCount() = _items.size
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val item = _items[position]
+        holder.binding.label.text = item
+        holder.binding.root.setOnClickListener { onRemove.invoke(item) }
+    }
+
+    class Holder(val binding: ItemFilterLabelBinding) : RecyclerView.ViewHolder(binding.root)
+}
