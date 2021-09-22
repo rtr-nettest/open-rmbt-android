91e811fb8116f1222f56745344a80be09e58ec23
michal cadrik <michal.cadrik@martes-specure.com>
2021-09-22 13:50:03 +0200

    ONT-3587 - Persistent client uuid - shown warning in the history and redirection to settings

--- nkom/src/main/java/at/rtr/rmbt/android/viewmodel/HistoryViewModel.kt
+++ nkom/src/main/java/at/rtr/rmbt/android/viewmodel/HistoryViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.paging.PagedList
 import at.rmbt.util.exception.HandledException
+import at.rtr.rmbt.android.config.AppConfig
 import at.rtr.rmbt.android.ui.viewstate.HistoryViewState
 import at.specure.data.entity.HistoryContainer
 import at.specure.data.repository.HistoryLoader
@@ -15,11 +16,11 @@ import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class HistoryViewModel @Inject constructor(private val repository: HistoryRepository, private val loader: HistoryLoader) : BaseViewModel() {
+class HistoryViewModel @Inject constructor(private val repository: HistoryRepository, private val loader: HistoryLoader, val config: AppConfig) : BaseViewModel() {
 
     private val _isLoadingLiveData = MutableLiveData<Boolean>()
 
-    val state = HistoryViewState()
+    val state = HistoryViewState(config)
 
     val isLoadingLiveData: LiveData<Boolean>
         get() = _isLoadingLiveData
@@ -31,6 +32,7 @@ class HistoryViewModel @Inject constructor(private val repository: HistoryReposi
         addStateSaveHandler(state)
 
         val isLoadingChannel = Channel<Boolean>()
+
         launch {
             isLoadingChannel.consumeAsFlow()
                 .debounce(200)
@@ -52,10 +54,6 @@ class HistoryViewModel @Inject constructor(private val repository: HistoryReposi
     }
 
     val activeFiltersLiveData = repository.appliedFiltersLiveData
-
-    init {
-        addStateSaveHandler(state)
-    }
 
     fun refreshHistory() {
         loader.refresh()
