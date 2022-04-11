625c0bd9c21d260bf8ecf55af2778cfb2338e52a
Michal Cadrik <michal.cadrik@specure.com>
2022-04-11 09:39:03 +0200

    Removed Huawei library dependencies

--- app/src/main/java/at/rtr/rmbt/android/map/wrapper/MarkerWrapper.kt
+++ app/src/main/java/at/rtr/rmbt/android/map/wrapper/MarkerWrapper.kt
@@ -3,24 +3,12 @@ package at.rtr.rmbt.android.map.wrapper
 import android.content.Context
 import androidx.annotation.DrawableRes
 import at.rtr.rmbt.android.util.iconFromVector
-import com.huawei.hms.maps.model.Marker
 
 interface MarkerWrapper {
 
     fun remove()
 
     fun setVectorIcon(context: Context, @DrawableRes iconResId: Int)
-}
-
-class HMSMarker(private val marker: Marker?) : MarkerWrapper {
-
-    override fun remove() {
-        marker?.remove()
-    }
-
-    override fun setVectorIcon(context: Context, iconResId: Int) {
-        marker?.iconFromVector(context, iconResId)
-    }
 }
 
 class GMSMarker(private val marker: com.google.android.gms.maps.model.Marker?) : MarkerWrapper {
--- app/src/main/java/at/rtr/rmbt/android/map/wrapper/TileOverlayWrapper.kt
+++ app/src/main/java/at/rtr/rmbt/android/map/wrapper/TileOverlayWrapper.kt
@@ -4,12 +4,6 @@ interface TileOverlayWrapper {
     fun remove()
 }
 
-class HMSOverlayWrapper(private val tileOverlay: com.huawei.hms.maps.model.TileOverlay?) : TileOverlayWrapper {
-    override fun remove() {
-        tileOverlay?.remove()
-    }
-}
-
 class GMSOverlayWrapper(private val tileOverlay: com.google.android.gms.maps.model.TileOverlay?) : TileOverlayWrapper {
     override fun remove() {
         tileOverlay?.remove()
