cf7f0a4ab6c8889a7fb52dfe78879c44ba9e8f08
Polina Gurina <76707268+polylina@users.noreply.github.com>
2026-06-01 16:31:32 +0200

    #143: fix crashes

--- app/src/main/java/at/rtr/rmbt/android/ui/activity/BaseActivity.kt
+++ app/src/main/java/at/rtr/rmbt/android/ui/activity/BaseActivity.kt
@@ -1,9 +1,12 @@
 package at.rtr.rmbt.android.ui.activity
 
 import android.annotation.TargetApi
+import android.app.PictureInPictureParams
 import android.content.res.Configuration
+import android.graphics.Point
 import android.os.Build
 import android.os.Bundle
+import android.util.Rational
 import android.view.View
 import android.view.WindowManager
 import androidx.annotation.LayoutRes
@@ -16,6 +19,8 @@ import at.rtr.rmbt.android.R
 import at.rtr.rmbt.android.ui.dialog.SimpleDialog
 import at.rtr.rmbt.android.viewmodel.BaseViewModel
 import timber.log.Timber
+import kotlin.math.max
+import kotlin.math.min
 
 private const val DIALOG_DEFAULT_OK = -1
 
@@ -87,5 +92,30 @@ abstract class BaseActivity : AppCompatActivity() {
         setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
         setWindowFlag(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS, true)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+
+    protected fun enterInPictureMode() {
+        var width: Int
+        var height: Int
+        var ratio = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val screenBounds = this.windowManager.maximumWindowMetrics.bounds
+            width = screenBounds.width()
+            height = screenBounds.height()
+            Rational(width, height)
+        } else {
+            val display = windowManager.defaultDisplay
+            val point = Point();
+            display.getSize(point);
+            width = point.x;
+            height = point.y;
+            Rational(width, height);
+        }
+        var ratioDouble = min(2.3, ratio.toDouble())
+        ratioDouble = max(0.5, ratioDouble)
+        if (ratioDouble != ratio.toDouble()) {
+            ratio = Rational((ratioDouble*10).toInt(), 10)
+        }
+        val pipBuilder = PictureInPictureParams.Builder().setAspectRatio(ratio)
+        enterPictureInPictureMode(pipBuilder.build())
     }
 }
